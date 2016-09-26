b633621dd3cdb3864de234d05e1c2b6b31f31ba5
yunfei <yunfei@yunfeis-iMac.local>
2016-09-26 17:21:31 +0800

    introduce vue-typescript to avoid typescript error

--- src/views/index-view/IndexView.ts
+++ src/views/index-view/IndexView.ts
@@ -1,14 +1,12 @@
 declare var Benchmark: any
 
 import testCases from '../../test-cases/testCases'
+import {VueComponent} from 'vue-typescript'
 
-export default {
+@VueComponent({
     template: require('./IndexView.html'),
-    style: require('./IndexView.less'),
-
-    data() {
-        return {
-            testCases: testCases
-        };
-    }
+    style: require('./IndexView.less')
+})
+export default class IndexView {
+    testCases = testCases
 }
--- src/views/test-case-view/TestCaseView.ts
+++ src/views/test-case-view/TestCaseView.ts
@@ -1,76 +1,70 @@
-declare var Benchmark: any
-declare var _: any
+declare var Benchmark: any;
+declare var _: any;
 
 import testCases from '../../test-cases/testCases'
+import {VueComponent} from 'vue-typescript'
 
-export default {
+@VueComponent({
     template: require('./TestCaseView.html'),
     style: require('./TestCaseView.less'),
 
-    data() {
+    created() {
         const testCaseName = decodeURIComponent(this.$route.params.testCaseName);
-        return {
-            testCase: testCases.filter(testCase => testCase.name === testCaseName)[0],
-            testResultItems: [],
-            testSummary: ''
-        };
+        this.testCase = testCases.filter(testCase => testCase.name === testCaseName)[0];
+        this.testResultItems = [];
+        this.testSummary = '';
     },
 
     beforeDestroy() {
         this.clearTestSuite();
-    },
+    }
+})
+export default class TestCaseView {
+    testCase = null
+    testResultItems = []
+    testSummary = ''
 
-    methods: {
-        clearTestSuite() {
-            if (this._testSuite) {
-                this._testSuite.abort();
-                this._testSuite.off();
-            }
-        },
+    _testSuite = null
 
-        getTestItemIdx(name) {
-            return _.findIndex(this.testCase.items, item => item.name === name);
-        },
-
-        runTest() {
-            this.clearTestSuite();
-
-            this.testResultItems = [];
-            this.testSummary = '';
-
-            var suite = this._testSuite = new Benchmark.Suite();
-            this.testCase.items.forEach(testItem => {
-                suite.add(testItem.name, testItem.func);
-            });
-
-            suite
-                .on('start', event => {
-                    const testResultItems = this.testResultItems;
-                    this.testCase.items.forEach((item, idx) => testResultItems[idx] = 'Executing...');
-                    this.testResultItems = testResultItems.concat();
-                })
-                .on('cycle', event => {
-                    const itemIdx = this.getTestItemIdx(event.target.name);
-                    if (itemIdx !== -1) {
-                        const testResultItems = this.testResultItems;
-                        testResultItems[itemIdx] = event.target.toString();
-                        this.testResultItems = testResultItems.concat();
-                    }
-                })
-                .on('complete', () => {
-                    this.addTestFinalSummary('Fastest is ' + suite.filter('fastest').map('name'));
-                })
-                .run({ 'async': true });
-        },
-
-        addTestResult(testResult:string) {
-            this.testResultItems.push(testResult);
-            console.log(testResult);
-        },
-
-        addTestFinalSummary(testSummary:string) {
-            this.testSummary = testSummary;
-            console.log(testSummary);
+    clearTestSuite() {
+        if (this._testSuite) {
+            this._testSuite.abort();
+            this._testSuite.off();
         }
+    }
+
+    getTestItemIdx(name) {
+        return _.findIndex(this.testCase.items, item => item.name === name);
+    }
+
+    runTest() {
+        this.clearTestSuite();
+
+        this.testResultItems = [];
+        this.testSummary = '';
+
+        var suite = this._testSuite = new Benchmark.Suite();
+        this.testCase.items.forEach(testItem => {
+            suite.add(testItem.name, testItem.func);
+        });
+
+        suite
+            .on('start', event => {
+                const testResultItems = this.testResultItems;
+                this.testCase.items.forEach((item, idx) => testResultItems[idx] = 'Executing...');
+                this.testResultItems = testResultItems.concat();
+            })
+            .on('cycle', event => {
+                const itemIdx = this.getTestItemIdx(event.target.name);
+                if (itemIdx !== -1) {
+                    const testResultItems = this.testResultItems;
+                    testResultItems[itemIdx] = event.target.toString();
+                    this.testResultItems = testResultItems.concat();
+                }
+            })
+            .on('complete', () => {
+                this.testSummary = 'Fastest is ' + suite.filter('fastest').map('name');
+            })
+            .run({ 'async': true });
     }
 }
